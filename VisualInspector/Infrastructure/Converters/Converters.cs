7e6c488d7b0cde8d3c7e808aa4498c65bb463dbd
GrushinEgor <egorgrushin@gmail.com>
2022-03-24 22:46:48 +0300

    Some UI changes

--- VisualInspector/Infrastructure/Converters/Converters.cs
+++ VisualInspector/Infrastructure/Converters/Converters.cs
@@ -9,7 +9,23 @@ using System.Windows.Markup;
 using System.Windows.Media;
 using VisualInspector.Models;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace VisualInspector.Infrastructure.Converters
 {
+    public class AdaptingWidthListBoxItemConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var listBox = value as ListBox;
+            var finalWidth = listBox.ActualWidth / listBox.Items.Count;
+            return finalWidth;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
